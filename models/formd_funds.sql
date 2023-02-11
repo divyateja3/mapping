4230380d4760e17c6248059aa9f9de1da970ef3d
KARTHIKEYA <surak504@gmail.com>
2023-02-11 12:52:23 +0530

    🪲 BUG: main.py script

--- models/formd_funds.sql
+++ models/formd_funds.sql
@@ -1,4 +1,4 @@
-SELECT firms_regulatoryfirm.firm_id, formd_firmformdvalue.entity_name AS "form_d_funds", firms_regulatoryfirm.cik_no AS "cik_no_fund" FROM formd_firmformdvalue
+SELECT firms_regulatoryfirm.firm_id AS "form_d_firm_id", formd_firmformdvalue.entity_name AS "form_d_funds", firms_regulatoryfirm.cik_no AS "cik_no_fund" FROM formd_firmformdvalue
 LEFT JOIN (
     SELECT firms_regulatoryfirm.id, firms_regulatoryfirm.firm_id, firms_firm.cik_no, firms_firm.crd_no FROM firms_regulatoryfirm
     LEFT JOIN firms_firm
